27c32294d67b360a3a1e988fe9ab14042174f17a
Furkan Baser <furkanbaserceng@outlook.com>
2022-05-18 16:10:43 +0300

    Data access codes are written for the InMemory environment.

---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,49 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    /// <summary>
+    /// Product nesnesi ile yapılacak CRUD işlemlerinin yapılacağı şablon interface.
+    /// </summary>
+    public interface IProductDal
+    {
+        /// <summary>
+        /// Tüm ürünleri listeleyecek metod şablonu.
+        /// Product classı, Entities katmanından geleceği için Entities'e referans verdim.
+        /// </summary>
+        /// <returns>Generic List tipinde tüm Product nesnelerini döndürecek.</returns>
+        List<Product> GetAll();
+
+        /// <summary>
+        /// Ürünleri, kategorilerine göre listeleyecek metod şablonu.
+        /// </summary>
+        /// <param name="categoryId"></param>
+        /// <returns></returns>
+        List<Product> GetByCategoryId(int categoryId);
+
+        /// <summary>
+        /// Parametre olarak gelen product nesnesini veritabanına ekleyecek metod şablonu.
+        /// </summary>
+        /// <param name="product"></param>
+        void Add(Product product);
+
+        /// <summary>
+        /// Parametre olarak gelen product nesnesini güncelleyip, veritabanına kaydedecek metod şablonu.
+        /// </summary>
+        /// <param name="product"></param>
+        void Update(Product product);
+
+        /// <summary>
+        /// Parametre olarak gelen product nesnesini silecek metod şablonu.
+        /// </summary>
+        /// <param name="product"></param>
+        void Delete(Product product);
+
+
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,116 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+
+        /// <summary>
+        /// Burada, veri tabanında çalışmayıp bellek üzerinde veri varmış gibi
+        /// fake veriler oluşturup çalışmalar yapacağım.
+        /// Class içi global _products oluşturdum.
+        /// InMemoryProductDal çağrıldığı anda constructorunda fake product ürünleri oluşturacak.
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            _products = new List<Product>
+            {
+                new Product
+                {
+                    ProductId=1,
+                    CategoryId=1,
+                    ProductName="Laptop",
+                    UnitsInStock=500,
+                    UnitPrice=17000
+                },
+                new Product
+                {
+                    ProductId=2,
+                    CategoryId=1,
+                    ProductName="Camera",
+                    UnitsInStock=300,
+                    UnitPrice=9600
+                },
+                new Product
+                {
+                    ProductId=3,
+                    CategoryId=2,
+                    ProductName="Mouse",
+                    UnitsInStock=1300,
+                    UnitPrice=120
+                },
+                new Product
+                {
+                    ProductId=4,
+                    CategoryId=3,
+                    ProductName="Keyboard",
+                    UnitsInStock=250,
+                    UnitPrice=560
+                }
+            };
+        }
+        
+
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        /// <summary>
+        /// Where() ile dönen sonuç IEnumerable tipinde olduğundan, sonucu listeye çevirmek gerekir.
+        /// </summary>
+        /// <param name="categoryId"></param>
+        /// <returns></returns>
+        public List<Product> GetByCategoryId(int categoryId)
+        {
+            return _products.Where(p => p.CategoryId == categoryId).ToList();
+        }
+
+
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        /// <summary>
+        /// Güncellenmek istenen referansı yakalayıp productToUpdate nesnesine aktardım.
+        /// Sonrasında güncellenecek değerleri gelen product nesnesiyle eşleştirdim.
+        /// </summary>
+        /// <param name="product"></param>
+        public void Update(Product product)
+        {
+
+            Product productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CategoryId = product.CategoryId;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
+        }
+
+        /// <summary>
+        /// Direkt _products.Remove(product) diyemeyiz.Referans tiplerle çalıştığımız için,
+        /// gelen productın ProductId si yakalanmalı ve referans üzerinden silinmeli.
+        /// Bu yüzden LINQ yapısını kullanarak PrimaryKey üzerinden ProductId yakalandı.
+        /// SingleOrDefault metodu ile _products listesi dönüldü ve eşleşme yakalanırsa silme işlemi gerçekleşecek.
+        /// </summary>
+        /// <param name="product"></param>
+        public void Delete(Product product)
+        {
+            
+            Product productToDelete = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            _products.Remove(productToDelete);
+        }
+
+        
+    }
+}
