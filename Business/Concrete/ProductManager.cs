567ed88f7c41428d73fc440c61fd4a6d24f26423
Furkan Baser <furkanbaserceng@outlook.com>
2022-05-18 16:43:08 +0300

    Codes have been written for the Business layer. The system works fine for Inmemory.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,31 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    /// <summary>
+    /// Business katmanında bir sınıfın instance ı oluşturulduğu anda oluşturulan sınıfa tightly-coupked(sıkı bağımlılık) oluşur.
+    /// Yazılımın sürdürülebilirliği için alternatif sistemlere geçildiğinde sıkıntı yaşanmaması adına 
+    /// veri erişim katmanının soyut nesnelerini construstora enjekte ederek weakly coupled bir ortam oluşturdum.
+    /// </summary>
+    public class ProductManager : IProductService
+    {
+        private IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+
+        }
+        public List<Product> GetAll()
+        {
+            return _productDal.GetAll();
+        }
+    }
+}
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,9 +1,19 @@
 ﻿
 
+using Business.Concrete;
+using DataAccess.Concrete.InMemory;
 
+ProductManager productManager = new ProductManager(new InMemoryProductDal());
 
+foreach (var item in productManager.GetAll())
+{
 
+    Console.WriteLine(item.ProductName);
+    Console.WriteLine(item.UnitPrice);
+    Console.WriteLine(item.UnitsInStock);
+    Console.WriteLine("----------------");
 
+}
 
 
 
