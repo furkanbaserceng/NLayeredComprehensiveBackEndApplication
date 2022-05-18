2541e1b4136810bb49709f4720e02f58360de6b8
Furkan Baser <furkanbaserceng@outlook.com>
2022-05-18 13:26:46 +0300

    Added entities layer objects.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Abstract
+{
+    /// <summary>
+    /// Bu abstract nesne; veritabanı nesnelerini gruplamak(imzalamak) için oluşturdum.
+    /// IEntity interfaceini implemente eden classları veritabanı tablosu olarak işaretledim.
+    /// </summary>
+    public interface IEntity
+    {
+
+    }
+}
---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,39 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    /// <summary>
+    /// Diğer tüm katmanların Product'a erişebilir olması için access modifiers'ını public yaptım.
+    /// Çünkü; DataAccess eklemek için,Business kontrol için, UI ise ürünü göstermek için ihtiyaç duyacak.
+    /// </summary>
+    public class Product : IEntity
+    {
+        /// <summary>
+        /// İlk aşamada salt kodlar yazacağım için Northwind veritabanındaki 
+        /// orijinal kolon adlarını yazdım.
+        /// </summary>
+        public int ProductId { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public string ProductName { get; set; }
+
+        /// <summary>
+        /// Northwind veritabanında UnitsInStock alanı smallint olarak tanımlı olduğu için
+        /// C# karşılığı olarak short tipi verdim.
+        /// </summary>
+        public short UnitsInStock { get; set; }
+
+        /// <summary>
+        /// Northwind veritabanında UnitsInStock alanı money olarak tanımlı olduğu için
+        /// C# karşılığı olarak decimal tipi verdim.
+        /// </summary>
+        public decimal UnitPrice { get; set; }
+
+    }
+}
