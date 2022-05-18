bd214df55aa1b26fe6fb7e4c34615bac20c12b47
Furkan Baser <furkanbaserceng@outlook.com>
2022-05-18 22:02:57 +0300

    (Refactoring)Generic constraints have been given to IEntityRepository.

--- DataAccess/Abstract/IEntityRepository.cs
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
@@ -7,7 +8,14 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Abstract
 {
-    public interface IEntityRepository<T>
+    /// <summary>
+    /// IEntityRepository'nin çalışacağı tip olan T parametresi class constrainti ile referans tip olmak zorunda.
+    /// Aynı zamanda IEntity interfacinden implemente edilmiş bir referans tip olmalı
+    /// ama bu kısıtla beraber IEntity nin kendisi de verilebilir.Bunu engellemek için new() kısıtı verdim.
+    /// Bu sayede sadece IEntity imzasına sahip olan ve instance ı alınabilen veritabanı nesneleri verilebilir hale geldi.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IEntityRepository<T> where T:class,IEntity,new()
     {
         List<T> GetAll(Expression<Func<bool,T>> filter=null);
         T Get(Expression<Func<bool, T>> filter);
