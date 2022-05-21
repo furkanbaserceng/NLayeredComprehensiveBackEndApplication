64629f43bd08c911efe72fd748515fbee68ace98
Furkan Baser <furkanbaserceng@outlook.com>
2022-05-21 19:16:29 +0300

    Added ASP.NET Web Api and ProductsController.The IOC structure was given in the Controllers and the system was tested.

---
+++ WebAPII/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPII.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+        private IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public List<Product> Get()
+        {
+            
+
+            var result=_productService.GetAll();
+
+            return result.Data;
+        }
+
+
+    }
+}
