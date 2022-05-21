ef70ec774b983ebaf980be47fcd1bd305305ca0a
Furkan Baser <furkanbaserceng@outlook.com>
2022-05-21 21:31:12 +0300

    Added to HttpPost operations for Products.

--- WebAPII/Controllers/ProductsController.cs
+++ WebAPII/Controllers/ProductsController.cs
@@ -30,8 +30,22 @@ namespace WebAPII.Controllers
             {
                 return Ok(result);
             }
-            return BadRequest(result.Message);
+            return BadRequest(result);
         }
+
+        [HttpPost]
+        public IActionResult Post(Product product)
+        {
+            var result = _productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result);
+
+            }
+            return BadRequest(result);
+
+        }
+
 
 
     }
