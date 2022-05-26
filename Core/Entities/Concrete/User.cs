e88a670f1afca2f0a82d655ed085d2a3ea53650a
Furkan Baser <furkanbaserceng@outlook.com>
2022-05-26 23:52:16 +0300

    Added Hash and Encryption operations.

---
+++ Core/Entities/Concrete/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities.Concrete
+{
+    public class User : IEntity
+    {
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        /// <summary>
+        /// db de binary alana karşılık geliyor byte[]
+        /// </summary>
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public bool Status { get; set; }
+
+
+    }
+
+}
