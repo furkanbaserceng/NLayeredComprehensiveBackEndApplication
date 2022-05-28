55dc26914f5363a99745040d654115de719ba6a5
Furkan Baser <furkanbaserceng@outlook.com>
2022-05-28 22:45:35 +0300

    Some fixes Cors configuration.

--- WebAPII/Program.cs
+++ WebAPII/Program.cs
@@ -77,7 +77,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseCors();
+app.UseCors(builder=>builder.WithOrigins("http:\\localhost:4200").AllowAnyHeader());
 app.UseHttpsRedirection();
 
 //app.UseAuthentication();
